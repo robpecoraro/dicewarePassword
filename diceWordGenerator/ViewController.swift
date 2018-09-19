54bf0ccb50f3143be87fa07d67c896677203cadc
Rob Pecoraro <rfpecoraro@gmail.com>
2018-09-19 08:55:08 -0400

    Initial Commit

---
+++ diceWordGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  diceWordGenerator
+//
+//  Created by admin on 9/19/18.
+//  Copyright © 2018 RobPecoraro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
